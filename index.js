7f752310ddcf4288cecd7b6dd92ced5b9d98775b
Parth Shirole <parthshirole06@gmail.com>
2022-10-16 12:05:08 +0530

    reminder msg tags a role

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new Client({
 */
 cron.schedule('00 12 * * 7', function() {
     const channel = client.channels.cache.get(process.env.CHANNEL_ID_ANNOUNCEMENTS);
-    channel.send('Update time! Provide your weekly updates to your respective mentors');
+    channel.send(`Update time <@&${process.env.ROLE_ID_MENTEES}>! Provide your weekly updates to your respective mentors`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
